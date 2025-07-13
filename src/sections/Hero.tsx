f7671c0506cf4c0d646f47f2f91791a5742a948a
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-07-13 22:05:58 +0530

    feat: enhance Hero section layout and improve text styling for better readability

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,25 +1,25 @@
 import Button from "@/components/Button";
 export default function Hero() {
   return (
-    <section className="px-3 py-24">
+    <section className="flex items-center justify-center px-3 py-24">
       <div className="container">
         <div className="flex justify-center">
           <div className="inline-flex rounded-full bg-gradient-to-r from-purple-400 to-pink-400 px-3 py-1 font-semibold text-neutral-950">
             ✨ $7.5M Seed round raised
           </div>
         </div>
-        <h1 className="mt-6 text-center text-6xl font-medium">
-          Impactful Design Created Effortlessly
+        <h1 className="mx-auto mt-6 max-w-5xl text-center text-6xl font-medium md:text-7xl lg:text-8xl">
+          Impactful Design, Created effortlessly
         </h1>
-        <p className="mt-8 text-center text-xl text-white/50">
+        <p className="mx-auto mt-8 max-w-2xl text-center text-xl text-white/50">
           Design without distractions. Fusion blends power and simplicity,
           helping you stay focused, fast, and fully in flow.
         </p>
-        <form className="mt-8 flex rounded-full border border-white/15 p-2">
+        <form className="mx-auto mt-8 flex max-w-lg rounded-full border border-white/15 p-2">
           <input
             type="email"
             placeholder="Enter Your email"
-            className="bg-transparent px-4"
+            className="bg-transparent px-4 md:flex-1"
           />
           <Button
             type="submit"
