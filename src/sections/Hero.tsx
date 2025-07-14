5c0f02e89eaaec587f0c73cb9da0e554b9700cab
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-07-14 22:10:13 +0530

    feat: enhance Hero section with image integration and improved layout

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,8 +1,25 @@
+import Image from "next/image";
 import Button from "@/components/Button";
+import designExample1Image from "@/assets/Images/Design-Ex-1.png";
+import designExample2Image from "@/assets/Images/Design-Ex-2.png";
 export default function Hero() {
   return (
     <section className="flex items-center justify-center px-3 py-24">
-      <div className="container">
+      <div className="relative container">
+        <div className="absolute top-16 -left-36">
+          <Image
+            src={designExample1Image}
+            alt="Design-Ex-1 image"
+            className="h-[500px] w-full"
+          />
+        </div>
+        <div className="absolute -top-16 -right-40">
+          <Image
+            src={designExample2Image}
+            alt="Design-Ex-2 image"
+            className="h-[500px] w-full"
+          />
+        </div>
         <div className="flex justify-center">
           <div className="inline-flex rounded-full bg-gradient-to-r from-purple-400 to-pink-400 px-3 py-1 font-semibold text-neutral-950">
             ✨ $7.5M Seed round raised
@@ -19,7 +36,7 @@ export default function Hero() {
           <input
             type="email"
             placeholder="Enter Your email"
-            className="bg-transparent px-4 md:flex-1"
+            className="bg-transparent px-4 outline-none md:flex-1"
           />
           <Button
             type="submit"
