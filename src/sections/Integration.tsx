451fc0d8fb8922ae541e7a131b55b40787838a9e
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-07-19 22:42:03 +0530

    chore: add logos for integration options

--- src/sections/Integration.tsx
+++ src/sections/Integration.tsx
@@ -1,3 +1,38 @@
+const Integrations = [
+  {
+    name: "Figma",
+    icon: "",
+    Description: "Figma is a collaborative interface design tools.",
+  },
+  {
+    name: "Notion",
+    icon: "",
+    Description: "Notion is all in one workspace for notes and docs.",
+  },
+  {
+    name: "Slack",
+    icon: "",
+    Description:
+      "Slack is a messaging platform for teams to collaborate and communicate in real time.",
+  },
+  {
+    name: "Relume",
+    icon: "",
+    Description:
+      "Relume helps you design and build websites faster with AI-powered wireframes and components.",
+  },
+  {
+    name: "Framer",
+    icon: "",
+    Description: "Framer is a professional website prototyping tool.",
+  },
+  {
+    name: "Github",
+    icon: "",
+    Description: "Github is the leading platform for code collaboration",
+  },
+];
+
 export default function Integration() {
   return <div>Integration</div>;
 }
