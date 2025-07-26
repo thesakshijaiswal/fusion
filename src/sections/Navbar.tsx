180311360ee013a4f5d5650b9c8714cec26e4d2f
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-07-26 20:40:53 +0530

    style: enhance Navbar section with sticky positioning and background styling

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -10,9 +10,9 @@ const navLinks = [
 
 export default function Navbar() {
   return (
-    <section className="py-4 lg:py-8">
+    <section className="sticky top-0 z-50 py-4 lg:py-8">
       <div className="container mx-auto max-w-5xl">
-        <div className="grid grid-cols-2 items-center rounded-full border border-white/15 p-2 px-4 md:pr-2 lg:grid-cols-3">
+        <div className="grid grid-cols-2 items-center rounded-full border border-white/15 bg-neutral-950/70 p-2 px-4 backdrop-blur-xs md:pr-2 lg:grid-cols-3">
           <div className="flex items-center gap-4">
             <Image src={LogoImage} alt="Fusion Logo" className="h-10 w-auto" />
             <h1 className="text-2xl font-extrabold tracking-wider">Fusion</h1>
