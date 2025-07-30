4165ff011efee025451d12738a165afa47146b94
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-07-30 16:25:29 +0530

    feat: create infinite scroller in LogoTicker with motion effects and improve link accessibility

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { Fragment } from "react";
 import Image from "next/image";
 import Logo1 from "@/assets/Images/algopipe.svg";
 import Logo2 from "@/assets/Images/be-commerce.svg";
@@ -6,6 +9,7 @@ import Logo4 from "@/assets/Images/rewarder.svg";
 import Logo5 from "@/assets/Images/tailxpense.png";
 import Logo6 from "@/assets/Images/mediGo.png";
 import Logo7 from "@/assets/Images/fusion.svg";
+import { motion } from "motion/react";
 
 const logos = [
   { name: "Algopipe", image: Logo1, href: "https://algopipe.vercel.app" },
@@ -26,7 +30,11 @@ const logos = [
   },
   { name: "TailXpense", image: Logo5, href: "https://tail-xpense.vercel.app" },
   { name: "Medigo", image: Logo6, href: "https://medigo-by-me.vercel.app" },
-  { name: "Fusion", image: Logo7, href: "" },
+  {
+    name: "Fusion",
+    image: Logo7,
+    href: "https://fusion-dashboard-ten.vercel.app",
+  },
 ];
 
 export default function LogoTicker() {
@@ -37,7 +45,7 @@ export default function LogoTicker() {
           Already chosen by these market leaders
         </h3>
         <div
-          className="mt-12 overflow-hidden"
+          className="mt-12 flex overflow-hidden"
           style={{
             maskImage:
               "linear-gradient(to right, transparent, black 10%, black 90%, transparent)",
@@ -45,23 +53,36 @@ export default function LogoTicker() {
               "linear-gradient(to right, transparent, black 10%, black 90%, transparent)",
           }}
         >
-          <div className="flex gap-24 pr-24">
-            {logos.map((logo) => (
-              <div
-                className="flex items-center justify-center gap-1 whitespace-nowrap"
-                key={logo.name}
-              >
-                <Image
-                  src={logo.image}
-                  alt={logo.name}
-                  className="h-20 w-20 object-contain"
-                />
-                <a href={logo.href} className="text-2xl font-semibold italic">
-                  {logo.name}
-                </a>
-              </div>
+          <motion.div
+            animate={{ x: "-50%" }}
+            transition={{ duration: 30, ease: "linear", repeat: Infinity }}
+            className="flex flex-none gap-24 pr-24"
+          >
+            {Array.from({ length: 2 }).map((_, i) => (
+              <Fragment key={i}>
+                {logos.map((logo) => (
+                  <div
+                    className="flex items-center justify-center gap-1 whitespace-nowrap"
+                    key={logo.name}
+                  >
+                    <Image
+                      src={logo.image}
+                      alt={logo.name}
+                      className="h-20 w-20 object-contain"
+                    />
+                    <a
+                      href={logo.href}
+                      className="text-2xl font-semibold italic"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {logo.name}
+                    </a>
+                  </div>
+                ))}
+              </Fragment>
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
