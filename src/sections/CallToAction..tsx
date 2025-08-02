c0993bf0269510f874769d02d41e845c411c97d6
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-08-02 20:37:14 +0530

    feat: add infinity scroll animation to CTA text

--- src/sections/CallToAction..tsx
+++ src/sections/CallToAction..tsx
@@ -1,15 +1,22 @@
+"use client";
+
+import { motion } from "motion/react";
 export default function CallToAction() {
   return (
     <section className="py-24">
       <div className="flex overflow-x-clip p-4">
-        <div className="flex flex-none gap-16 text-7xl font-medium">
+        <motion.div
+          animate={{ x: "-50%" }}
+          transition={{ duration: 30, ease: "linear", repeat: Infinity }}
+          className="flex flex-none gap-16 text-7xl font-medium"
+        >
           {Array.from({ length: 10 }).map((_, i) => (
             <div key={i} className="flex items-center gap-16">
               <span className="text-lime-400">&#10038;</span>
               <span>Try it for free</span>
             </div>
           ))}
-        </div>
+        </motion.div>
       </div>
     </section>
   );
